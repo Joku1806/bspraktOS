fe25061dd931bb15df11e1234f7f4836b65113e3
Jonas Kuß <jonaskuss@protonmail.com>
2021-10-26 11:09:08 +0200

    Vorgabe auf neuesten Stand aktualisieren

--- kernel/start.c
+++ kernel/start.c
@@ -1,17 +1,21 @@
-#include <arch/bsp/kprintf.h>
 #include <arch/bsp/yellow_led.h>
 
 volatile unsigned int counter = 0;
 
-void increment_counter() { counter++; }
+void increment_counter() {
+	counter++;
+}
 
-void start_kernel() {
 
-  yellow_on();
-  kprintf("Hello from kernel land! %c", 'A');
+void start_kernel(){
 
-  // Endless counter
-  for (;;) {
-    increment_counter();
-  }
+	yellow_on();
+
+
+	// Endless counter
+	for (;;) {
+		increment_counter();
+	}
+
+
 }
