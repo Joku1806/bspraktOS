d2da3936fd34204283302293ab80416f2f0b4ca6
Jonas Kuß <jonaskuss@protonmail.com>
2021-10-28 12:03:52 +0200

    Grundfunktionen für den PL001 Treiber fertigstellen.

--- include/kernel/drivers/pl001.h
+++ include/kernel/drivers/pl001.h
@@ -1,7 +1,36 @@
 #ifndef PL001_H
 #define PL001_H
 
-char pl001_receive();
+#include <stdint.h>
+
+#define UART_BASE (0x7E201000 - 0x3F000000)
+
+// wir brauchen vermutlich nicht alle -- nutzlose noch entfernen
+typedef enum {
+  DR = 0x0,
+  FR = 0x18,
+  IBRD = 0x24,
+  FBRD = 0x28,
+  LCRH = 0x2c,
+  CR = 0x30,
+  IFLS = 0x34,
+  IMSC = 0x38,
+  RIS = 0x3c,
+  MIS = 0x40,
+  ICR = 0x44,
+  DMACR = 0x48,
+  ITCR = 0x80,
+  ITIP = 0x84,
+  ITOP = 0x88,
+  TDR = 0x8c
+} register_offsets;
+
+typedef enum {
+  BUSY = 1 << 3,
+  RXFE = 1 << 7,
+} FR_FLAGS;
+
+int8_t pl001_receive();
 void pl001_send(char ch);
 
 #endif
--- kernel/drivers/pl001.c
+++ kernel/drivers/pl001.c
@@ -1,7 +1,23 @@
-char pl001_receive() {
-  // TODO
+#include <kernel/drivers/pl001.h>
+
+volatile uint32_t *pl001_register(register_offsets offset) {
+  return (volatile uint32_t *)(UART_BASE + offset);
+}
+
+void pl001_wait_until_transmission_complete() {
+  while (*pl001_register(FR) & BUSY) {
+  }
+}
+
+int8_t pl001_receive() {
+  // TODO: is that the right name for the flag?
+  if (!(*pl001_register(FR) & RXFE)) {
+    return -1;
+  }
+  return *pl001_register(DR);
 }
 
 void pl001_send(char ch) {
-  // TODO
+  pl001_wait_until_transmission_complete();
+  *pl001_register(DR) = ch;
 }
