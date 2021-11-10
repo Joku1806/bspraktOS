bdf852c2d3bd1a7455054c4f878d068b37494a2e
Jonas Kuß <jonaskuss@protonmail.com>
2021-11-10 17:01:39 +0100

    Lösche Tests aus der 1. Aufgabe.

--- kernel/start.c
+++ kernel/start.c
@@ -1,27 +1,9 @@
 #include "kernel/kprintf.h"
-#include <arch/bsp/yellow_led.h>
+#include <arch/bsp/pl001.h>
 #include <config.h>
-#include <kernel/drivers/pl001.h>
-
-volatile unsigned int counter = 0;
-
-void increment_counter() { counter++; }
 
 void start_kernel() {
-
-  yellow_on();
-  test_kprintf();
-
   // Endless counter
   for (;;) {
-    increment_counter();
-    int8_t ch = pl001_receive();
-    if (ch < 0) {
-      continue;
-    }
-
-    kprintf("Es wurde folgender Buchstabe eingegeben: %c, In Hexadezimal: %x, "
-            "In Dezimal: %08u\n",
-            ch, ch, ch);
   }
 }
