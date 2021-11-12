f36838c66b81505c78c86b6a9fe035cf730e9f8d
Jonas Kuß <jonaskuss@protonmail.com>
2021-11-12 13:15:01 +0100

    Versuche einen Prefetch Abort Interrupt auszulösen.

--- kernel/start.c
+++ kernel/start.c
@@ -1,9 +1,11 @@
-#include "kernel/kprintf.h"
 #include <arch/bsp/pl001.h>
 #include <config.h>
+#include <kernel/kprintf.h>
 
 void start_kernel() {
-  // Endless counter
+  kprintf("Booted!\n");
+  asm volatile("bkpt #0");
+  kprintf("After Prefetch Abort!\n");
   for (;;) {
   }
 }
