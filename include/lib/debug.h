5f8a7a0d74000c4725898f99b72649254a629028
Jonas Kuß <jonaskuss@protonmail.com>
2021-11-17 11:31:21 +0100

    Schreibe ein Log-Level-System für debug.h.
    In Dateien kann jetzt ein Log-Level (DEBUG_LEVEL, WARN_LEVEL, PANIC_LEVEL)
    VOR! #include <lib/debug.h> definiert werden, was für die gesamte
    Translation Unit gilt. Das heißt, dass jetzt verschiedene Module
    voneinander unabhängige Warnstufen haben können. Wenn WARN_LEVEL definiert wird,
    werden nur warnln() und panicln(), aber nicht dbgln() ausgegeben.
    Die Warnstufen sind mit Macros implementiert, also wird alles schon
    zur Kompilierzeit aufgelöst und unter dem Level nicht ausgebbare Sachen
    sind gar nicht mehr im Programm enthalten. Ziemlich nice!

--- include/lib/debug.h
+++ include/lib/debug.h
@@ -9,7 +9,15 @@
 #define PANIC_COLOR "\033[31;1m"
 #define RESET_COLOR "\033[0m"
 
-#ifdef DEBUG
+#define PANIC_LEVEL 0
+#define WARNING_LEVEL 1
+#define DEBUG_LEVEL 2
+
+#ifndef LOG_LEVEL
+#define LOG_LEVEL DEBUG_LEVEL
+#endif
+
+#if LOG_LEVEL >= DEBUG_LEVEL
 #define dbgln(...)                                                             \
   do {                                                                         \
     kprintf(DEBUG_COLOR "[*] " RESET_COLOR);                                   \
@@ -20,18 +28,27 @@
 #define dbgln(...)
 #endif
 
+#if LOG_LEVEL >= WARNING_LEVEL
 #define warnln(...)                                                            \
   do {                                                                         \
     kprintf(WARN_COLOR "[WARNING] " RESET_COLOR);                              \
     kprintf(__VA_ARGS__);                                                      \
     kprintf("\n");                                                             \
   } while (0)
+#else
+#define warnln(...)
 #endif
 
-#define panic(...)                                                             \
+#if LOG_LEVEL >= PANIC_LEVEL
+#define panicln(...)                                                           \
   do {                                                                         \
     kprintf(PANIC_COLOR "[PANIC] " RESET_COLOR);                               \
     kprintf(__VA_ARGS__);                                                      \
     kprintf("\n");                                                             \
     halt_cpu()                                                                 \
   } while (0)
+#else
+#define panic(...)
+#endif
+
+#endif
