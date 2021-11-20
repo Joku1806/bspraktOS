e7c411366a0bb78e3b06c0630332a3cb379d7894
Jonas Kuß <jonaskuss@protonmail.com>
2021-11-20 14:41:56 +0100

    Blocken anstatt -1 zurückzugeben.

--- arch/bsp/pl001.c
+++ arch/bsp/pl001.c
@@ -4,19 +4,12 @@ volatile uint32_t *pl001_register(register_offsets offset) {
   return (volatile uint32_t *)(UART_BASE + offset);
 }
 
-void pl001_wait_until_transmission_complete() {
-  while (*pl001_register(FR) & TXFF) {}
-}
-
 int8_t pl001_receive() {
-  if (*pl001_register(FR) & RXFE) {
-    return -1;
-  }
-
+  while (*pl001_register(FR) & RXFE) {}
   return *pl001_register(DR);
 }
 
 void pl001_send(char ch) {
-  pl001_wait_until_transmission_complete();
+  while (*pl001_register(FR) & TXFF) {}
   *pl001_register(DR) = ch;
 }
