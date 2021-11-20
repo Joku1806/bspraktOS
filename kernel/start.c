1ed8d221e4f8f8f92971edf04abbb6b268ff786f
Jonas Kuß <jonaskuss@protonmail.com>
2021-11-20 14:44:41 +0100

    Verschiedene Interrupts bei Tastatureingabe auslösen.

--- kernel/start.c
+++ kernel/start.c
@@ -1,10 +1,25 @@
+#include "arch/cpu/mission_control.h"
 #include <arch/bsp/pl001.h>
 #include <config.h>
 #include <kernel/kprintf.h>
 
 void start_kernel() {
-  kprintf("Booted!\n");
-  asm volatile("bkpt #0");
-  kprintf("After Prefetch Abort!\n");
-  for (;;) {}
+  for (;;) {
+    char ch = pl001_receive();
+
+    switch (ch) {
+      case 's':
+        asm volatile("svc #0");
+        break;
+      case 'a':
+        asm volatile("mov r0, #0x1 \n ldr r0, [r0]");
+        break;
+      case 'u':
+        asm volatile(".word 0xf7f0a000\n");
+        break;
+      case 'p':
+        asm volatile("bkpt #0");
+        break;
+    }
+  }
 }
