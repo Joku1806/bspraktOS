f8821eb4aefef9891e2c040731961b86d7c2b4e6
Jonas Kuß <jonaskuss@protonmail.com>
2021-11-23 12:23:33 +0100

    Initialisiere systimer in der BCM2835 Peripherie.
    Der Timer Interrupt wird jetzt erfolgreich ausgelöst.
    Behandelt wird allerdings noch nichts, wir landen deswegen
    in einer Endlosschleife :D

---
+++ include/arch/bsp/interrupt_peripherals.h
@@ -0,0 +1,32 @@
+#ifndef INTERRUPT_PERIPHERALS_H
+#define INTERRUPT_PERIPHERALS_H
+
+#define INTERRUPT_PERIPHERALS_BASE (0x7E00B000 - 0x3F000000)
+
+#ifdef __ASSEMBLER__
+
+#define ENABLE_IRQS_1_OFFSET 0x210
+#define TIMER1_ENABLE_FLAG 0x2
+
+#else
+
+typedef enum {
+  IRQ_basic_pending = 0x200,
+  IRQ_pending_1 = 0x204,
+  IRQ_pending_2 = 0x208,
+  FIQ_control = 0x20C,
+  enable_IRQs_1 = 0x210,
+  enable_IRQs_2 = 0x214,
+  enable_basic_IRQs = 0x218,
+  disable_IRQs_1 = 0x21C,
+  disable_IRQs_2 = 0x220,
+  disable_basic_IRQs = 0x224
+} peripherals_register_offsets;
+
+typedef enum {
+  timer_pending = 0x1 << 0,
+} IRQ_basic_pending_flags;
+
+#endif
+
+#endif
