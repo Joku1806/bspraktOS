2f5fcb1045ca5ee8bd6e748b12a29ba7b0e1aab9
Jonas Kuß <jonaskuss@protonmail.com>
2021-11-25 22:24:41 +0100

    Stack defines in bsp verschieben, weils da besser hinpasst. Ändere außerdem die Stackgröße auf 1kB.

--- include/arch/bsp/stack_defines.h
+++ include/arch/bsp/stack_defines.h
@@ -5,9 +5,9 @@
 // FIXME: vielleicht off-by-one?
 #define STACK_TOP_ADDRESS 128 * 1024 * 1024
 
-// Jeder Stack kriegt fürs erste 1MB, vielleicht ein bisschen
+// Jeder Stack kriegt fürs erste 1kB, vielleicht ein bisschen
 // übertrieben, aber den Platz haben wir im Moment noch :^)
-#define STACK_SIZE 1024 * 1024
+#define STACK_SIZE 1024
 
 // Stackpointer für die verschiedenen Interrupt-Handler.
 #define USER_SYSTEM_SP STACK_TOP_ADDRESS
