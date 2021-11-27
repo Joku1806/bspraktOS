626d70ad9dbe9d866382d15d32c7c200f24f3571
Jonas Kuß <jonaskuss@protonmail.com>
2021-11-27 19:26:37 +0100

    sleep_ms und einige Konvertierungsfunktionen hinzufügen. Dieser Change kann noch nicht benutzt werden, weil wegen fehlenden Floatingpoint Instruktionen nicht kompiliert werden kann.

--- include/lib/timing.h
+++ include/lib/timing.h
@@ -1,3 +1,6 @@
 #include <stddef.h>
 
+size_t mhz_to_milliseconds(size_t mhz);
+size_t milliseconds_to_mhz(size_t ms);
+void sleep_milliseconds(size_t ms);
 void sleep_mhz(size_t mhz);
--- lib/timing.c
+++ lib/timing.c
@@ -1,6 +1,19 @@
 #include <arch/bsp/systimer.h>
 #include <lib/timing.h>
 
+size_t mhz_to_milliseconds(size_t mhz) {
+  return (size_t)(mhz / (SYSTIMER_FREQUENCY_HZ * 1000.0));
+}
+
+size_t milliseconds_to_mhz(size_t ms) {
+  return (size_t)(ms * SYSTIMER_FREQUENCY_HZ / 1000.0);
+}
+
+void sleep_milliseconds(size_t ms) {
+  size_t initial = systimer_value();
+  while (systimer_value() < initial + milliseconds_to_mhz(ms)) {}
+}
+
 void sleep_mhz(size_t mhz) {
   size_t initial = systimer_value();
   while (systimer_value() < initial + mhz) {}
