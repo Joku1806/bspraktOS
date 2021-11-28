4be4e053914a06f7d46f04deef2550a732a42312
Jonas Kuß <jonaskuss@protonmail.com>
2021-11-28 13:27:47 +0100

    Ausgabe von Zeichen im Unterprogramm noch schneller machen!

--- include/config.h
+++ include/config.h
@@ -22,7 +22,7 @@
 
 #ifdef __QEMU__
 /* Werte zum testen unter QEMU */
-#define BUSY_WAIT_COUNTER 300000
+#define BUSY_WAIT_COUNTER 100000
 #else
 /* Werte zum testen auf der Hardware */
 #define BUSY_WAIT_COUNTER 30000
