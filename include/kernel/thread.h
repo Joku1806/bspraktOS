4eaf0a77aa80f48a193c5a9450d42c22e89b8839
Jonas Kuß <jonaskuss@protonmail.com>
2021-12-07 22:14:53 +0100

    Verschiebe alles in bounded_thread_list.c/h nach thread.c/h

--- include/kernel/thread.h
+++ include/kernel/thread.h
@@ -1,6 +1,11 @@
 #ifndef THREAD_H
 #define THREAD_H
 
+#include <stddef.h>
+#include <stdint.h>
+
+#define TCB_LIST_SIZE 32
+
 typedef enum {
   ready = 0,
   waiting = 1,
@@ -8,6 +13,20 @@ typedef enum {
   finished = 3,
 } thread_status;
 
+typedef struct node {
+  struct node *previous;
+  struct node *next;
+} node;
+
+typedef struct {
+  node *node;
+  uint32_t regs[16];
+  uint32_t cpsr;
+  size_t index;
+} tcb;
+void thread_list_initialise();
+node *get_thread_list_head(thread_status status);
+void transfer_thread_block_to_list(node *tcb_node, node *list);
 void thread_cleanup();
 
 #endif
--- kernel/thread.c
+++ kernel/thread.c
@@ -1,9 +1,73 @@
-#include <kernel/bounded_thread_list.h>
+#include <arch/bsp/stack_defines.h>
+#include <arch/cpu/psr.h>
+#include <arch/cpu/registers.h>
 #include <kernel/scheduler.h>
+#include <kernel/thread.h>
+#include <lib/assertions.h>
+#include <stddef.h>
+#include <stdint.h>
 
-void thread_cleanup(){
+extern void schedule_thread();
+static tcb blocks[TCB_LIST_SIZE];
 
-  transfer_node_to_list(get_thread_list_head(running), get_thread_list_head(finished));
-  schedule_next_thread();
-  
+node *ready_head = NULL;
+node *waiting_head = NULL;
+node *running_head = NULL;
+node *finished_head = (node *)&blocks[0];
+
+void thread_list_initialise() {
+  for (size_t i = 0; i < TCB_LIST_SIZE; i++) {
+    node *current = (node *)&blocks[i];
+    if (i > 0) {
+      current->previous = (node *)&blocks[i - 1];
+    } else {
+      current->previous = (node *)&blocks[TCB_LIST_SIZE - 1];
+    }
+
+    if (i < TCB_LIST_SIZE - 1) {
+      current->next = (node *)&blocks[i + 1];
+    } else {
+      current->next = (node *)&blocks[0];
+    }
+
+    reset_thread_context(i);
+  }
+}
+
+node *get_thread_list_head(thread_status status) {
+  switch (status) {
+    case ready:
+      return ready_head;
+    case waiting:
+      return waiting_head;
+    case running:
+      return running_head;
+    case finished:
+      return finished_head;
+  }
+}
+
+void transfer_thread_block_to_list(node *tcb_node, node *list) {
+  VERIFY(tcb_node != NULL && list != NULL);
+
+  if (tcb_node->previous != NULL) {
+    tcb_node->previous->next = tcb_node->next;
+  }
+
+  if (tcb_node->next != NULL) {
+    tcb_node->next->previous = tcb_node->previous;
+  }
+
+  if (list == NULL) {
+    list = tcb_node;
+    tcb_node->previous = tcb_node;
+    tcb_node->next = tcb_node;
+  } else {
+    tcb_node->previous = list;
+    tcb_node->next = list->next;
+
+    list->next->previous = tcb_node;
+    list->next = tcb_node;
+  }
+}
 }
