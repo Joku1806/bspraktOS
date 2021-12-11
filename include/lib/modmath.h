175f099283d3c1dbdd719f274b030f40144cf0ab
Jonas Kuß <jonaskuss@protonmail.com>
2021-12-11 23:27:27 +0100

    Fehlerhaftes MODULO_SUB vorerst beheben. Ist aber nicht optimal, weil x und i <= m sein müssen. Für MODULO_ADD gibt es diese Beschränkung nicht.

--- include/lib/modmath.h
+++ include/lib/modmath.h
@@ -1,2 +1,3 @@
 #define MODULO_ADD(x, i, m) (((x) + (i)) % (m))
-#define MODULO_SUB(x, i, m) ((x) == 0 ? (m)-1 : (i)-1)
+// nimmt an, dass x, i <= m sind
+#define MODULO_SUB(x, i, m) ((x) >= (i) ? (x) - (i) : (m) - (i) + (x))
