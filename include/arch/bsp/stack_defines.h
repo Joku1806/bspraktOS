c62964c3902a1c22510f18914bb48cd6dd20bbd4
Jonas Kuß <jonaskuss@protonmail.com>
2021-12-11 10:49:20 +0100

    Unnötige Kommentare entfernen.

--- include/arch/bsp/stack_defines.h
+++ include/arch/bsp/stack_defines.h
@@ -3,11 +3,8 @@
 
 // höchste Adresse für 128MB physischen Speicher
 #define STACK_TOP_ADDRESS 128 * 1024 * 1024 - 8
-
-// Jeder Stack kriegt fürs erste 1kB
 #define STACK_SIZE 1024 * 4
 
-// Stackpointer für die verschiedenen Interrupt-Handler.
 #define USER_SYSTEM_SP STACK_TOP_ADDRESS
 #define SUPERVISOR_SP STACK_TOP_ADDRESS - STACK_SIZE * 1
 #define ABORT_SP STACK_TOP_ADDRESS - STACK_SIZE * 2
