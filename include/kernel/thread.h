d6cbd132e212f5b128e9a90c4335fe81bc5517db
Jonas Kuß <jonaskuss@protonmail.com>
2021-12-11 15:02:53 +0100

    Unnötige Deklarationen löschen.

--- include/kernel/thread.h
+++ include/kernel/thread.h
@@ -33,11 +33,6 @@ void reset_thread_context(size_t index);
 void save_thread_context(tcb *thread, uint32_t *regs, uint32_t cpsr);
 void load_thread_context(tcb *thread, uint32_t *current_thread_regs);
 void thread_list_initialise();
-// FIXME: Sollten die in einer extra Funktion kombiniert werden? Im Moment
-// kommen sie nur zusammen vor. Außerdem sollte thread_block -> node umbenannt
-// werden und diese Funktionalität in lib abgekapselt werden.
-void remove_node_from_current_list(node *thread);
-void append_node_to_list(node *thread, node **list);
 void thread_cleanup();
 
 #endif
