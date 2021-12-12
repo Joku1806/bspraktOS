442386be95a04536d1bcc8d89a26d246f9329be1
Jonas Kuß <jonaskuss@protonmail.com>
2021-12-12 13:02:15 +0100

    Fehlerhaftes Stackpointer Alignment beheben.

--- kernel/thread.c
+++ kernel/thread.c
@@ -88,10 +88,13 @@ void thread_create(void (*func)(void *), const void *args,
 
   tcb *thread = (tcb *)&finished_head;
 
-  // 8-byte align
-  thread->regs[SP_POSITION] -= args_size + (8 % (args_size % 8));
-  thread->regs[PC_POSITION] = (uint32_t)func;
+  thread->regs[SP_POSITION] -= args_size;
   memcpy((void *)thread->regs[SP_POSITION], args, args_size);
+  if (args_size % 8 != 0) { // 8-byte align
+    thread->regs[SP_POSITION] -= 8 - args_size % 8;
+  }
+
+  thread->regs[PC_POSITION] = (uint32_t)func;
 
   remove_node_from_current_list((node *)thread);
   append_node_to_list((node *)thread,
