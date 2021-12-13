6d14c931bc4866dc92ab9325b092fb689dca9996
Jonas Kuß <jonaskuss@protonmail.com>
2021-12-13 22:42:33 +0100

    Füge Funktion hinzu, mit der geprüft werden kann, ob die Instruktion an einer Adresse ein syscall (svc) ist.

--- include/kernel/syscall.h
+++ include/kernel/syscall.h
@@ -1,10 +1,12 @@
 #ifndef SYSCALL_H
 #define SYSCALL_H
 
+#include <stdbool.h>
 #include <stdint.h>
 
 #define SYSCALL_EXIT_NO 1
 
+bool is_syscall(uint32_t instruction_address);
 uint32_t get_syscall_no(uint32_t svc_instruction_address);
 
 void sys$exit();
--- kernel/syscall.c
+++ kernel/syscall.c
@@ -5,4 +5,8 @@ uint32_t get_syscall_no(uint32_t svc_instruction_address) {
   return *(uint32_t *)svc_instruction_address & 0x00ffffff;
 }
 
+bool is_syscall(uint32_t instruction_address) {
+  return (*(uint32_t *)instruction_address & 0xff000000) == 0xef000000;
+}
+
 void sys$exit() { asm volatile("svc %0 \n\t" ::"I"(SYSCALL_EXIT_NO)); }
