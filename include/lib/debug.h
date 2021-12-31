26e02727c4fbb87c40d776bb5d2b6a5ac979cce2
Jonas Kuß <jonaskuss@protonmail.com>
2021-12-31 02:00:39 +0100

    Angabe von Debugfarben mittels hsl implementieren.
    Das macht es sehr einfach, Farben anhand von
    Eigenschaften anstatt randomly auszuwählen.
    
    Es kann jetzt z.B. nicht mehr passieren, dass
    ausversehen eine Farbe gewählt wird, die zu
    dunkel zum lesen ist. Außerdem können keine
    Farbtöne gewählt werden, die den warning-
    und panic-Farben zu ähnlich sind.
    
    Was noch verbessert werden könnte, ist die
    Farbverteilung: Im Moment werden hauptsächlich
    noch Blau- und Grüntöne gewählt. Dafür bräuchte
    es eine bessere Hashfunktion (vllt aus uthash?)

--- include/lib/debug.h
+++ include/lib/debug.h
@@ -8,10 +8,6 @@
 #include <stdint.h>
 #include <string.h>
 
-#define WARNING_COLOR "\033[33;1m"
-#define PANIC_COLOR "\033[31;1m"
-#define RESET_COLOR "\033[0m"
-
 #define PANIC_LEVEL 0
 #define WARNING_LEVEL 1
 #define DEBUG_LEVEL 2
@@ -24,82 +20,122 @@
 #define LOG_LABEL "Unknown"
 #endif
 
-#define COLORED_OUTPUT true
+#ifndef LOG_COLORED_OUTPUT
+#define LOG_COLORED_OUTPUT true
+#endif
 
-// inspiriert von https://stackoverflow.com/questions/8317508/hash-function-for-a-string
-#define P1 54059
-#define P2 76963
-#define SEED_RED 37
-#define SEED_GREEN 59
-#define SEED_BLUE 83
+#if LOG_COLORED_OUTPUT
 
-inline uint8_t label_color_hash(unsigned long seed) {
-  unsigned long h = seed;
-  for (size_t i = 0; i < sizeof(LOG_LABEL) - 1; i++) {
-    h = (h * P1) ^ (LOG_LABEL[i] * P2);
-  }
+#include <lib/color.h>
+#include <lib/fraction.h>
+#include <lib/hash.h>
+#include <limits.h>
 
-  // wir machen uns damit ein bisschen die Verteilungsqualität
-  // kaputt, aber floating point Skalierung können wir leider
-  // nicht benutzen :/ Vielleicht fällt mir noch eine bessere
-  // Lösung ein.
-  return h % UINT8_MAX;
-}
+#define ESC_RESET_COLOR "\033[0m"
+
+static const rgb_color debug_warning_color = {.red = 249, .green = 115, .blue = 31};
+static const rgb_color debug_panic_color = {.red = 236, .green = 55, .blue = 19};
+
+static rgb_color module_color;
+static bool initialised_module_color = false;
+
+#define HUE_SEED 37
+#define SATURATION_SEED 59
+#define LIGHTNESS_SEED 83
+
+#define INITIALISE_MODULE_COLOR_ONCE()                                                             \
+  do {                                                                                             \
+    if (!initialised_module_color) {                                                               \
+      hsl_color hsl = {                                                                            \
+          .hue = project_long_onto_range(hash_string(LOG_LABEL, HUE_SEED), 60, 330),               \
+          .saturation = project_long_onto_range(hash_string(LOG_LABEL, SATURATION_SEED), 70, 100), \
+          .lightness = project_long_onto_range(hash_string(LOG_LABEL, LIGHTNESS_SEED), 60, 80),    \
+      };                                                                                           \
+      module_color = hsl_to_rgb(&hsl);                                                             \
+      initialised_module_color = true;                                                             \
+    }                                                                                              \
+  } while (0)
+
+#define PRINT_COLORED(str, color)                                                           \
+  do {                                                                                      \
+    kprintf("\033[38;2;%u;%u;%um" str ESC_RESET_COLOR, color.red, color.green, color.blue); \
+  } while (0)
+
+#define PRINT_COLORED_LABEL()                        \
+  do {                                               \
+    INITIALISE_MODULE_COLOR_ONCE();                  \
+    PRINT_COLORED("(" LOG_LABEL ") ", module_color); \
+  } while (0)
+
+#endif
 
 #if LOG_LEVEL >= DEBUG_LEVEL
-#define dbgln(...)                                                            \
-  do {                                                                        \
-    if (COLORED_OUTPUT) {                                                     \
-      uint8_t cr = label_color_hash(SEED_RED);                                \
-      uint8_t cg = label_color_hash(SEED_GREEN);                              \
-      uint8_t cb = label_color_hash(SEED_BLUE);                               \
-      kprintf("\033[38;2;%u;%u;%um(" LOG_LABEL ") " RESET_COLOR, cr, cg, cb); \
-    } else {                                                                  \
-      kprintf("(" LOG_LABEL ") ");                                            \
-    }                                                                         \
-                                                                              \
-    kprintf(__VA_ARGS__);                                                     \
-    kprintf("\n");                                                            \
+
+#if LOG_COLORED_OUTPUT
+#define dbgln(...)         \
+  do {                     \
+    PRINT_COLORED_LABEL(); \
+    kprintf(__VA_ARGS__);  \
+    kprintf("\n");         \
   } while (0)
+#else
+#define dbgln(...)               \
+  do {                           \
+    kprintf("(" LOG_LABEL ") "); \
+    kprintf(__VA_ARGS__);        \
+    kprintf("\n");               \
+  } while (0)
+#endif
+
 #else
 #define dbgln(...)
 #endif
 
 #if LOG_LEVEL >= WARNING_LEVEL
-#define warnln(...)                                                                                                  \
-  do {                                                                                                               \
-    if (COLORED_OUTPUT) {                                                                                            \
-      uint8_t cr = label_color_hash(SEED_RED);                                                                       \
-      uint8_t cg = label_color_hash(SEED_GREEN);                                                                     \
-      uint8_t cb = label_color_hash(SEED_BLUE);                                                                      \
-      kprintf(WARNING_COLOR "[WARNING] " RESET_COLOR "\033[38;2;%u;%u;%um(" LOG_LABEL ") " RESET_COLOR, cr, cg, cb); \
-    } else {                                                                                                         \
-      kprintf("[WARNING] (" LOG_LABEL ") ");                                                                         \
-    }                                                                                                                \
-                                                                                                                     \
-    kprintf(__VA_ARGS__);                                                                                            \
-    kprintf("\n");                                                                                                   \
+
+#if LOG_COLORED_OUTPUT
+#define warnln(...)                                   \
+  do {                                                \
+    PRINT_COLORED("[WARNING] ", debug_warning_color); \
+    PRINT_COLORED_LABEL();                            \
+    kprintf(__VA_ARGS__);                             \
+    kprintf("\n");                                    \
   } while (0)
+#else
+#define warnln(...)                        \
+  do {                                     \
+    kprintf("[WARNING] (" LOG_LABEL ") "); \
+    kprintf(__VA_ARGS__);                  \
+    kprintf("\n");                         \
+  } while (0)
+#endif
+
 #else
 #define warnln(...)
 #endif
 
 #if LOG_LEVEL >= PANIC_LEVEL
-#define panicln(...)                                                                                             \
-  do {                                                                                                           \
-    if (COLORED_OUTPUT) {                                                                                        \
-      uint8_t cr = label_color_hash(SEED_RED);                                                                   \
-      uint8_t cg = label_color_hash(SEED_GREEN);                                                                 \
-      uint8_t cb = label_color_hash(SEED_BLUE);                                                                  \
-      kprintf(PANIC_COLOR "[PANIC] " RESET_COLOR "\033[38;2;%u;%u;%um(" LOG_LABEL ") " RESET_COLOR, cr, cg, cb); \
-    } else {                                                                                                     \
-      kprintf("[PANIC] (" LOG_LABEL ") ");                                                                       \
-    }                                                                                                            \
-                                                                                                                 \
-    kprintf(__VA_ARGS__);                                                                                        \
-    kprintf("\n");                                                                                               \
-    halt_cpu();                                                                                                  \
+
+#if LOG_COLORED_OUTPUT
+#define panicln(...)                              \
+  do {                                            \
+    PRINT_COLORED("[PANIC] ", debug_panic_color); \
+    PRINT_COLORED_LABEL();                        \
+    kprintf(__VA_ARGS__);                         \
+    kprintf("\nHalting System.\n");               \
+    halt_cpu();                                   \
   } while (0)
+#else
+#define panicln(...)                     \
+  do {                                   \
+    kprintf("[PANIC] (" LOG_LABEL ") "); \
+    kprintf(__VA_ARGS__);                \
+    kprintf("\nHalting System.\n");      \
+    halt_cpu();                          \
+  } while (0)
+
+#endif
+
 #else
 #define panic(...)
 #endif
