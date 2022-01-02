297bdb35246958b6d1f6ea271dc70ed009a867ab
Jonas Kuß <jonaskuss@protonmail.com>
2022-01-02 15:57:26 +0100

    Formatierung

--- lib/timing.c
+++ lib/timing.c
@@ -1,9 +1,9 @@
-#include <stddef.h>
 #define LOG_LEVEL WARNING_LEVEL
 #define LOG_LABEL "Timing"
 
 #include <arch/bsp/systimer.h>
 #include <lib/timing.h>
+#include <stddef.h>
 
 size_t milliseconds_to_mhz(size_t ms) {
   return ms * SYSTIMER_FREQUENCY_HZ / 1000;
@@ -21,6 +21,7 @@ void sleep_mhz(size_t mhz) {
 
 void sleep_macgyver(size_t instrs) {
   for (volatile size_t i = 0; i < instrs; i++) {
-    asm volatile("" ::: "memory");
+    asm volatile("" ::
+                     : "memory");
   }
 }
