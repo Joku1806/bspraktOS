eec3c167d839a2492e4f3fa02248009806b0c4d5
Jonas Kuß <jonaskuss@protonmail.com>
2022-01-02 17:28:12 +0100

    Preview für Modulfarben hinzufügen, um schnell Farbänderungen testen zu können.

--- kernel/start.c
+++ kernel/start.c
@@ -13,6 +13,34 @@
 #include <stdbool.h>
 #include <stddef.h>
 
+void preview_module_colors() {
+  const char *modules[] = {
+      "Unknown",
+      "Interrupt",
+      "Scheduler",
+      "Kernel Start",
+      "Thread",
+      "Color",
+      "Intrusive List",
+      "Ringbuffer",
+      "Timing",
+      "Userthread"};
+
+  kprintf("Previewing Module Colors\n");
+  for (size_t i = 0; i < sizeof(modules) / sizeof(const char *); i++) {
+    long hue_hash = clamp_unsigned(hash_string_with_seed(modules[i], HUE_SEED), 0, LONG_MAX);
+    long saturation_hash = clamp_unsigned(hash_string_with_seed(modules[i], SATURATION_SEED), 0, LONG_MAX);
+    long lightness_hash = clamp_unsigned(hash_string_with_seed(modules[i], LIGHTNESS_SEED), 0, LONG_MAX);
+    hsl_color hsl = {
+        .hue = project_long_onto_range(hue_hash, 0, 359),
+        .saturation = project_long_onto_range(saturation_hash, 70, 90),
+        .lightness = project_long_onto_range(lightness_hash, 50, 80),
+    };
+    rgb_color module_color = hsl_to_rgb(&hsl);
+    kprintf("\033[38;2;%u;%u;%um%s\033[0m\n", module_color.red, module_color.green, module_color.blue, modules[i]);
+  }
+}
+
 void print_menu() {
   kprintf("Willkommen in unserem Betriebssystem!\n"
           "Interrupts ==========================\n"
@@ -34,4 +62,5 @@ void start_kernel() {
   pl001_setup();
   thread_list_initialise();
   print_menu();
+  preview_module_colors();
 }
