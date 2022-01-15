197661b2715ba5770a3db47d98acdfbd2ab9f150
Jonas Kuß <jonaskuss@protonmail.com>
2022-01-15 20:20:21 +0100

    Thread abstürzen, wenn 's' eingegeben wurde und richtige Timingfunktionen benutzen.

--- user/main.c
+++ user/main.c
@@ -2,6 +2,7 @@
 #define LOG_LABEL "Userthread"
 
 #define NUM_CALCULATION_CYCLES 20
+#define SLEEP_TIME_MS 1000
 
 #include <config.h>
 #include <stddef.h>
@@ -16,13 +17,19 @@ void printer(void *x) {
   char ch = *(char *)x;
   dbgln("Successfully started user thread with char argument '%c'!", ch);
 
+  if (ch == 's') {
+    // random bullshit go
+    asm volatile("svc #0x1337 \t\n" ::
+                     :);
+  }
+
   for (int i = 0; i < NUM_CALCULATION_CYCLES; i++) {
     sys$output_character(ch);
 
     if (is_uppercase(ch)) {
-      sleep_macgyver(BUSY_WAIT_COUNTER);
+      sleep_milliseconds(SLEEP_TIME_MS);
     } else {
-      sys$stall_thread(mhz_to_milliseconds(BUSY_WAIT_COUNTER));
+      sys$stall_thread(SLEEP_TIME_MS);
     }
   }
 }
