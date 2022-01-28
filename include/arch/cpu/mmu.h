ef0a74a73222e73cc9bd94ba148584c9a0ed8bc4
Jonas Kuß <jonaskuss@protonmail.com>
2022-01-28 15:23:14 +0100

    Reihenfolge der Bitfelder in Section Entries umkehren.

--- include/arch/cpu/mmu.h
+++ include/arch/cpu/mmu.h
@@ -6,69 +6,69 @@
 #define L1_TABLE_SIZE 0x1000
 
 typedef struct __attribute__((__packed__)) {
-  uint32_t unused : 30;
   uint32_t l1_entry_type_fault : 2;
+  uint32_t unused : 30;
 } l1_fault;
 
 typedef struct __attribute__((__packed__)) {
-  uint32_t base_address : 12;
-  uint32_t NS : 1;
-  uint32_t pad0_unset : 1;
-  uint32_t nG : 1;
-  uint32_t S : 1;
-  uint32_t AP0 : 1;
-  uint32_t TEX : 3;
-  uint32_t AP1 : 2;
-  uint32_t IMPL : 1;
-  uint32_t domain : 4;
-  uint32_t XN : 1;
-  uint32_t C : 1;
-  uint32_t B : 1;
-  uint32_t pad1_set : 1;
   uint32_t PXN : 1;
+  uint32_t pad1_set : 1;
+  uint32_t B : 1;
+  uint32_t C : 1;
+  uint32_t XN : 1;
+  uint32_t domain : 4;
+  uint32_t IMPL : 1;
+  uint32_t AP1_0 : 2;
+  uint32_t TEX : 3;
+  uint32_t AP2 : 1;
+  uint32_t S : 1;
+  uint32_t nG : 1;
+  uint32_t pad0_unset : 1;
+  uint32_t NS : 1;
+  uint32_t base_address : 12;
 } l1_section;
 
 typedef struct __attribute__((__packed__)) {
-  uint32_t base_address : 22;
-  uint32_t IMPL : 1;
-  uint32_t domain : 4;
-  uint32_t SBZ : 1;
-  uint32_t NS : 1;
-  uint32_t PXN : 1;
-  uint32_t pad0_unset : 1;
   uint32_t pad1_set : 1;
+  uint32_t pad0_unset : 1;
+  uint32_t PXN : 1;
+  uint32_t NS : 1;
+  uint32_t SBZ : 1;
+  uint32_t domain : 4;
+  uint32_t IMPL : 1;
+  uint32_t base_address : 22;
 } l2_handle;
 
 typedef struct __attribute__((__packed__)) {
-  uint32_t unused : 30;
   uint32_t l2_entry_type_fault : 2;
+  uint32_t unused : 30;
 } l2_fault;
 
 typedef struct __attribute__((__packed__)) {
-  uint32_t base_address : 16;
-  uint32_t XN : 1;
-  uint32_t TEX : 3;
-  uint32_t nG : 1;
-  uint32_t S : 1;
-  uint32_t AP0 : 1;
-  uint32_t SBZ : 3;
-  uint32_t AP1 : 2;
-  uint32_t C : 1;
-  uint32_t B : 1;
   uint32_t l2_entry_type_lp : 2;
+  uint32_t B : 1;
+  uint32_t C : 1;
+  uint32_t AP1_0 : 2;
+  uint32_t SBZ : 3;
+  uint32_t AP2 : 1;
+  uint32_t S : 1;
+  uint32_t nG : 1;
+  uint32_t TEX : 3;
+  uint32_t XN : 1;
+  uint32_t base_address : 16;
 } l2_large_page;
 
 typedef struct __attribute__((__packed__)) {
-  uint32_t base_address : 20;
-  uint32_t nG : 1;
-  uint32_t S : 1;
-  uint32_t AP0 : 1;
-  uint32_t TEX : 3;
-  uint32_t AP1 : 2;
-  uint32_t C : 1;
-  uint32_t B : 1;
-  uint32_t pad1_set : 1;
   uint32_t XN : 1;
+  uint32_t pad0_set : 1;
+  uint32_t B : 1;
+  uint32_t C : 1;
+  uint32_t AP1_0 : 2;
+  uint32_t TEX : 3;
+  uint32_t AP2 : 1;
+  uint32_t S : 1;
+  uint32_t nG : 1;
+  uint32_t base_address : 20;
 } l2_small_page;
 
 typedef union {
@@ -85,7 +85,6 @@ typedef union {
   uint32_t packed;
 } l1_entry;
 
-l1_entry get_full_access_entry();
-void initialise_l1_table();
+void mmu_configure();
 
 #endif
